8973bf84261752c591877554415892aee84ab2db
soloda1 <2023-01986@students.ssau.ru>
2026-04-11 14:05:05 +0400

    feat: обновил ридми

--- tests/widget.page.ts
+++ tests/widget.page.ts
@@ -5,6 +5,7 @@ enum WidgetPageSelectors {
     WIDGET_BODY = '[class^=widgetWrapper] > [class^=widget__]',
     HEADER_TEXT = 'header h5',
     BUTTON_OPEN = '[data-test=openWidget]',
+    // BUTTON_WRITE_TO_US = '[class^=btn]', 
     BUTTON_WRITE_TO_US = '[data-test="button_feedback_form"]',
     ARTICLE_POPULAR_TITLE = '[class^=popularTitle__]',
     ARTICLE_POPULAR_LIST = `${ARTICLE_POPULAR_TITLE} + ul[class^=articles__]`,
